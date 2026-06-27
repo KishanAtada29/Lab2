153d51d9a105fe701bad2975f18df7a95965934d
Kishan Atada <katada@student.cscc.edu>
2026-06-27 02:53:10 -0400

    Add the main class and created two objects from Player class

---
+++ Lab9_katada-1.py
@@ -0,0 +1,20 @@
+from player import Player
+
+class main:
+
+    player1 = Player("Player 1")
+    player2 = Player("Player 2")
+
+    no_play_again = 'n'
+
+
+    while no_play_again.casefold() != 'n':
+        player1.toss_coin()
+        player2.toss_coin()
+
+        player1_side = player1.get_coin_side()
+        player2_side = player2.get_coin_side()
+
+        print(f"{player1.get_name} tossed {player1_side}")
+        print(f"{player2.get_name()} tossed {player2_side}")
+
