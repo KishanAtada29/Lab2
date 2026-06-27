fcc27a3692631284bd19b4456855622e201ea24d
Kishan Atada <katada@student.cscc.edu>
2026-06-27 02:33:47 -0400

    Set the 'Head' as defualt attribute for __sideup in coin class. Removed the returns to add assigments in toss method

--- coin.py
+++ coin.py
@@ -1,14 +1,13 @@
 import random
 class Coin:
-    def __init__(self, sideup):
-        self._sideup = sideup
+    def __init__(self):
+        self.__sideup = "Head"
     
     def toss(self):
-        self._sideup = random.randint(0,1)
-        if self._sideup == 1:
-            return 'Head'
+        if random.randint(0,1) == 1:
+            self.__sideup = "Tails"
         else:
-            return 'Tails'
+            self.__sideup = "Heads"
     
     def get_sideup(self):
         return self._sideup
